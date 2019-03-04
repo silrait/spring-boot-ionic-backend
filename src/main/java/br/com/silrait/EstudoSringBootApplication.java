232f1edca47d2cdcdc924e0fb50b425043de3c18
silrait <thiarllisba@hotmail.com>
2019-03-04 12:20:00 -0300

    Instanciando umas categorias para teste

--- src/main/java/br/com/silrait/EstudoSringBootApplication.java
+++ src/main/java/br/com/silrait/EstudoSringBootApplication.java
@@ -1,13 +1,30 @@
 package br.com.silrait;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudoSringBootApplication {
+import br.com.silrait.domain.Categoria;
+import br.com.silrait.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudoSringBootApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
