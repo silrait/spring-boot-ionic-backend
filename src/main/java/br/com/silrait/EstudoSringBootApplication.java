0cc854ef6a84159a70a71a2290172b2658530af8
silrait <thiarllisba@hotmail.com>
2019-03-04 10:53:03 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/silrait/EstudoSringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.silrait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSringBootApplication.class, args);
+	}
+
+}
