c91d863ccef8282999f2c4717f86253ca3605148
silrait <thiarllisba@hotmail.com>
2019-03-20 16:33:04 -0300

    Personalizando o retorno da validação

--- src/main/java/br/com/silrait/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/silrait/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +24,18 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err = new ValidationError(HttpStatus.NOT_FOUND.value(), "Erro de validação", System.currentTimeMillis());
+		
+		e.getBindingResult().getFieldErrors().forEach( fieldError -> {
+			err.addError( fieldError.getField(), fieldError.getDefaultMessage());
+		});
+		
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
