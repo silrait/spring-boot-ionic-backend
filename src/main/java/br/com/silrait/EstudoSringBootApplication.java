10f5b2d3a722a8f09f489f0f7c377063e762bef6
silrait <thiarllisba@hotmail.com>
2019-03-20 15:25:12 -0300

    Paginação com parâmetros opcionais na requisição.

--- src/main/java/br/com/silrait/EstudoSringBootApplication.java
+++ src/main/java/br/com/silrait/EstudoSringBootApplication.java
@@ -61,8 +61,13 @@ public class EstudoSringBootApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		p1.addCategoria(cat1);
