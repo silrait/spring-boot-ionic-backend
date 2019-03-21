80a07e28ddd6277b4fab1e579e886c4234629282
silrait <thiarllisba@hotmail.com>
2019-03-21 17:52:14 -0300

    PUR, DELETE E GET para Cliente

--- src/main/java/br/com/silrait/resources/ClienteResource.java
+++ src/main/java/br/com/silrait/resources/ClienteResource.java
@@ -1,13 +1,24 @@
 package br.com.silrait.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.silrait.domain.Cliente;
+import br.com.silrait.dto.ClienteDTO;
 import br.com.silrait.services.ClienteService;
 
 @RestController
@@ -21,5 +32,50 @@ public class ClienteResource {
 		Cliente c = service.find(id);
 		
 		return ResponseEntity.ok(c);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> lista = service.findAll();
+		List<ClienteDTO> listaDTO = lista.stream().map( categoria -> new ClienteDTO(categoria) )
+			.collect(Collectors.toList());
+		return ResponseEntity.ok(listaDTO);
+	}
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> lista = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listaDTO = lista.map(categoria -> new ClienteDTO(categoria));
+		return ResponseEntity.ok(listaDTO);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO clienteDTO) {
+		Cliente categoria = service.fromDTO(clienteDTO);
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Integer id) {
+		Cliente categoria = service.fromDTO(clienteDTO);
+		categoria.setId(id);
+		categoria = service.update(categoria);
+
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/silrait/services/ClienteService.java
+++ src/main/java/br/com/silrait/services/ClienteService.java
@@ -1,12 +1,20 @@
 package br.com.silrait.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.silrait.domain.Cliente;
+import br.com.silrait.domain.Cliente;
+import br.com.silrait.dto.ClienteDTO;
 import br.com.silrait.repositories.ClienteRepository;
+import br.com.silrait.services.exceptions.DataIntegrityException;
 import br.com.silrait.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,5 +27,43 @@ public class ClienteService {
 		
 		return optional.orElseThrow(() -> new ObjectNotFoundException("Objeto Não encontrado! Id:" + id 
 				+ ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return repo.save(cliente);
+	}
+
+	public Cliente update(Cliente cliente) {
+		Cliente cliDB = find(cliente.getId());
+		updateData(cliDB, cliente);
+		return repo.save(cliDB);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente referenciado por outras entidades");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente cliDB, Cliente cliUPD) {
+		cliDB.setNome(cliUPD.getNome());
+		cliDB.setEmail(cliUPD.getEmail());
 	}
 }
