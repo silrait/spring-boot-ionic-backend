9703976518ec5b4844606a6faadb840ddab3dc6f
silrait <thiarllisba@hotmail.com>
2019-03-25 17:20:29 -0300

    Criando profile de desenvolvimento

--- src/main/java/br/com/silrait/config/DevConfig.java
+++ src/main/java/br/com/silrait/config/DevConfig.java
@@ -1,6 +1,7 @@
 package br.com.silrait.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,13 +9,20 @@ import org.springframework.context.annotation.Profile;
 import br.com.silrait.services.DBService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws Exception {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
