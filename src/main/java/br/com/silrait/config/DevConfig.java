0f467180399aee40b670292a94be752d1dbe1dad
silrait <thiarllisba@hotmail.com>
2019-03-27 05:02:02 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/silrait/config/DevConfig.java
+++ src/main/java/br/com/silrait/config/DevConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.silrait.services.DBService;
+import br.com.silrait.services.EmailService;
+import br.com.silrait.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -25,5 +27,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
